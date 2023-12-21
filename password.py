1ea0a365fb9de0f45f083909eb07c0880c889b82
TruptiNinghot22 <140891147+TruptiNinghot22@users.noreply.github.com>
2023-12-21 14:30:41 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,4 +1,4 @@
-'''import random
+import random
 letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numbers=['0','1','2','3','4','5','6','7','8','9']
 symbols=['!','@','#','$','&','*','(',')']
@@ -48,22 +48,5 @@ for i in range (n):
     value=input("Enter a value")
     dic[key]=value
 print(dic)
-'''
-
-#Operator overloading example
-class Book:
-    def __init__(self,title,pages):
-        self.title=title
-        self.pages=pages
-    def __add__(self,other):
-        total=self.pages+other.pages
-        return Book('All books', total)
-    def __str__(self):
-        return str(self.pages)
-b1=Book('One indian girl', 300)
-b2=Book('making india awesome', 200)
-b3=Book('half girl', 400)
-b4=Book('full', 300)
-print('total number of pages', b1+b2+b3+b4)
 
 
